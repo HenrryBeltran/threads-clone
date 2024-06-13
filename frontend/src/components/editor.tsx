231d28c159aade72dc9902cf05d49946505fd902
Henrry Beltrán <henrry26bt@gmail.com>
2024-06-12 19:04:04 -0500

    Still figthing with the editor but I'm really close

--- frontend/src/components/editor.tsx
+++ frontend/src/components/editor.tsx
@@ -5,72 +5,17 @@ type CursorPosition = {
   end: number;
 };
 
-const initialHTMLLine = '<p class="whitespace-pre-wrap"><br/></p>';
-
 export function Editor() {
-  const [htmlContent, setHtmlContent] = useState<string>(initialHTMLLine);
+  const [text, setText] = useState("");
+  const [insertKey, setInsertKey] = useState("");
   const contentEditableRef = useRef<HTMLDivElement>(null);
 
   function handleKeyDown(e: React.KeyboardEvent<HTMLDivElement>) {
-    const element = e.currentTarget;
-    const selection = window.getSelection();
-    const range = selection?.getRangeAt(0);
-
-    if (!range) return;
-
-    if (e.key === "Enter") {
-      e.preventDefault();
-
-      const br = document.createElement("br");
-
-      const containerIsBR = range.commonAncestorContainer.nodeName === "BR";
-
-      if (!containerIsBR) {
-        range.insertNode(br);
-      }
-
-      if (
-        br.previousSibling?.nodeType === Node.TEXT_NODE &&
-        (!br.nextSibling?.nodeValue || br.nextSibling?.nodeValue.length < 1)
-      ) {
-        const extraBr = document.createElement("br");
-        br.after(extraBr);
-        selection?.setPosition(extraBr);
-      } else if (containerIsBR) {
-        (range.commonAncestorContainer as Element).after(br);
-        selection?.setPosition(br);
-      } else {
-        selection?.setPosition(br.nextSibling);
-      }
-    }
-
-    if (e.key === "Backspace") {
-      const parent = selectCursorParent();
-
-      if (parent?.textContent?.length === 1) {
-        e.preventDefault();
-        parent.innerHTML = "<br/>";
-      }
-
-      if (element.textContent === "" && element.children.length === 1) {
-        holdAnEmptyLine();
-      }
-
-      if (e.metaKey || (e.altKey && element.textContent && element.textContent.split(" ").length < 2)) {
-        holdAnEmptyLine();
-      }
-    }
-
-    function holdAnEmptyLine() {
-      e.preventDefault();
-      e.currentTarget.innerHTML = initialHTMLLine;
-      selection?.setPosition(element.children[0] as HTMLElement);
-    }
+    setInsertKey(e.key);
   }
 
   function handleInput(e: React.FormEvent<HTMLDivElement>) {
-    const value = e.currentTarget.innerHTML;
-    setHtmlContent(value);
+    setText(e.currentTarget.innerText);
   }
 
   function highlightText(text: string) {
@@ -102,7 +47,7 @@ export function Editor() {
 
     const result = highlightedLines.join("<br>");
 
-    return result;
+    return `<p class="whitespace-pre-wrap">${result}</p>`;
   }
 
   useEffect(() => {
@@ -112,16 +57,21 @@ export function Editor() {
   }, []);
 
   useEffect(() => {
+    console.log("~ text >>>", text);
+
     if (contentEditableRef.current) {
       const el = contentEditableRef.current;
-      const p = el.childNodes[0] as HTMLElement;
       const cursorPosition = saveCursorPosition(el);
 
-      p.innerHTML = highlightText(p.innerText || p.textContent || "");
+      el.innerHTML = highlightText(text);
 
       restoreCursorPosition(el, cursorPosition);
+
+      if (insertKey === "Enter") {
+        moveCursorOneLineDown();
+      }
     }
-  }, [htmlContent]);
+  }, [text]);
 
   return (
     <div
@@ -139,26 +89,47 @@ export function Editor() {
   );
 }
 
-function selectCursorParent() {
+/// TODO: the enter and backspace keys have bugs. But we are more close than other times.
+
+// function getCurrentLineNumber(): number {
+//   const selection = window.getSelection();
+//
+//   if (!selection || selection.rangeCount === 0) return 1; // Default to line 1 if no selection
+//
+//   const range = selection.getRangeAt(0);
+//   const startContainer = range.startContainer;
+//
+//   if (!startContainer) return 1; // Default to line 1 if no start container
+//
+//   let lineNumber = 1;
+//   let currentNode: Node | null = startContainer.parentNode;
+//
+//   // Traverse up the DOM tree from the start container to the contentEditable element
+//   while (currentNode && currentNode !== document.body) {
+//     if (currentNode.nodeName === 'DIV' && currentNode.isSameNode(contentEditableRef.current)) {
+//       break; // Break if we reach the contentEditable element
+//     }
+//     if (currentNode.nodeName === 'BR') {
+//       lineNumber++;
+//     }
+//     currentNode = currentNode.parentNode;
+//   }
+//
+//   return lineNumber;
+// }
+
+function moveCursorOneLineDown() {
   const selection = window.getSelection();
 
-  if (!selection || selection.rangeCount === 0) return null;
+  if (!selection || selection.rangeCount === 0) return;
 
   const range = selection.getRangeAt(0);
-  let element = range.startContainer as HTMLElement;
 
-  if (element.nodeType === Node.TEXT_NODE) {
-    element = element.parentElement as HTMLElement;
-  }
-
-  if (element.tagName === "DIV") {
-    element = element.children[0] as HTMLElement;
-  }
-
-  return element;
+  console.log("~ selection", selection);
+  console.log("~ range", range);
 }
 
-const saveCursorPosition = (el: HTMLElement): CursorPosition | null => {
+function saveCursorPosition(el: HTMLElement): CursorPosition | null {
   const selection = window.getSelection();
 
   if (!selection || selection.rangeCount === 0) return null;
@@ -174,7 +145,7 @@ const saveCursorPosition = (el: HTMLElement): CursorPosition | null => {
     start,
     end: start + range.toString().length,
   };
-};
+}
 
 function restoreCursorPosition(el: HTMLElement, positions: CursorPosition | null) {
   if (!positions) return;
@@ -202,218 +173,102 @@ function restoreCursorPosition(el: HTMLElement, positions: CursorPosition | null
     return null;
   }
 
-  function setCursorPosition(el: HTMLElement, cursorPosition: CursorPosition) {
-    const selection = window.getSelection();
+  const selection = window.getSelection();
+  if (!selection) return;
 
-    if (!selection) return;
+  const start = createRange(el, { count: positions.start });
+  const end = createRange(el, { count: positions.end });
 
-    const start = createRange(el, { count: cursorPosition.start });
-    const end = createRange(el, { count: cursorPosition.end });
+  if (start && end) {
+    const range = document.createRange();
+    range.setStart(start.node, start.offset);
+    range.setEnd(end.node, end.offset);
 
-    if (start && end) {
-      const range = document.createRange();
-      range.setStart(start.node, start.offset);
-      range.setEnd(end.node, end.offset);
-
-      selection.removeAllRanges();
-      selection.addRange(range);
-    }
+    selection.removeAllRanges();
+    selection.addRange(range);
   }
-
-  setCursorPosition(el, positions);
 }
 
-// import { useEffect, useRef, useState } from "react";
-//
-// type CursorPosition = {
-//   start: number;
-//   end: number;
-// };
-//
-// const initialHTMLLine = '<p class="whitespace-pre-wrap"><br/></p>';
-//
-// export function Editor() {
-//   const [htmlContent, setHtmlContent] = useState<string>(initialHTMLLine);
-//   const contentEditableRef = useRef<HTMLDivElement>(null);
-//
-//   function handleKeyDown(e: React.KeyboardEvent<HTMLDivElement>) {
-//     const element = e.currentTarget;
-//     const selection = window.getSelection();
-//     const range = selection?.getRangeAt(0);
-//
-//     if (range === null || range === undefined) return;
-//
-//     if (e.key === "Enter") {
-//       e.preventDefault();
-//
-//       const br = document.createElement("br");
-//       br.id = "new";
-//
-//       const containerIsBR = range.commonAncestorContainer.nodeName === "BR";
-//
-//       if (!containerIsBR) {
-//         range.insertNode(br);
-//       }
-//
-//       if (
-//         br.previousSibling?.nodeType === Node.TEXT_NODE &&
-//         (br.nextSibling?.nodeValue?.length === undefined || br.nextSibling?.nodeValue?.length < 1)
-//       ) {
-//         const extraBr = document.createElement("br");
-//         extraBr.id = "extra";
-//         br.after(extraBr);
-//         selection?.setPosition(extraBr);
-//       } else if (containerIsBR) {
-//         (range.commonAncestorContainer as Element).after(br);
-//         selection?.setPosition(br);
-//       } else {
-//         selection?.setPosition(br.nextSibling);
-//       }
-//     }
-//
-//     if (e.key === "Backspace") {
-//       const parent = selectCursorParent();
-//
-//       if (parent?.textContent?.length === 1) {
-//         e.preventDefault();
-//         parent.innerHTML = "<br/>";
-//       }
-//     }
-//
-//     function holdAnEmptyLine() {
-//       e.preventDefault();
-//       e.currentTarget.innerHTML = initialHTMLLine;
-//
-//       selection?.setPosition(element.children[0]!);
-//     }
-//
-//     if (e.key === "Backspace" && element.textContent === "" && element.children.length === 1) {
-//       holdAnEmptyLine();
-//     }
-//
-//     if (e.key === "Backspace" && e.metaKey) {
-//       holdAnEmptyLine();
-//     }
-//
-//     if (e.key === "Backspace" && e.altKey && element.textContent && element.textContent.split(" ").length < 2) {
-//       holdAnEmptyLine();
-//     }
-//   }
-//
-//   function handleInput(e: React.FormEvent<HTMLDivElement>) {
-//     const value = e.currentTarget.innerText;
-//     setHtmlContent(value);
-//   }
-//
-//   function highlightText(text: string) {
-//     if (text.length === 0) {
-//       return "<br/>";
-//     }
-//
-//     const hashtagPattern = /#(\w+)/g;
-//     const mentionPattern = /@(\w+)/g;
-//
-//     // Split text into lines to handle line breaks
-//     const lines = text.split("\n");
-//
-//     const highlightedLines = lines.map((line) => {
-//       // Replace hashtags and mentions with highlighted spans
-//       const highlightedLine = line
-//         .replace(hashtagPattern, '<span class="text-blue-500" data-lexical-text="true">#$1</span>')
-//         .replace(mentionPattern, '<span class="text-blue-500" data-lexical-text="true">@$1</span>');
-//
-//       // Split by the highlighted parts to wrap normal text in spans
-//       const parts = highlightedLine.split(/(<span.*?<\/span>)/g);
-//
-//       // Wrap normal text parts in spans
-//       const wrappedParts = parts.map((part) => {
-//         if (part.match(/<span.*?<\/span>/)) {
-//           return part;
-//         }
-//         return part ? `<span data-lexical-text="true">${part}</span>` : "";
-//       });
-//
-//       return wrappedParts.join("");
-//     });
-//
-//     // Filter out empty lines and join with <br> to form the final HTML string
-//     const result = highlightedLines.filter((line) => line.trim() !== "").join("<br>");
-//
-//     return result;
-//   }
-//
-//   useEffect(() => {
-//     if (contentEditableRef.current) {
-//       contentEditableRef.current.focus();
-//     }
-//   }, []);
-//
-//   useEffect(() => {
-//     if (contentEditableRef.current) {
-//       const el = contentEditableRef.current;
-//       const p = el.childNodes[0] as HTMLParagraphElement;
-//       const cursorPosition = saveCursorPosition(el);
-//
-//       console.log("~ line text", p.innerText);
-//       p.innerHTML = highlightText(p.innerText);
-//
-//       restoreCursorPosition(el, cursorPosition);
-//     }
-//   }, [htmlContent]);
-//
-//   return (
-//     <div
-//       ref={contentEditableRef}
-//       contentEditable
-//       suppressContentEditableWarning={true}
-//       onKeyDown={handleKeyDown}
-//       onInput={handleInput}
-//       className="h-full w-full border border-gray-300 outline-none"
-//     >
-//       <p className="whitespace-pre-wrap">
-//         <br />
-//       </p>
-//     </div>
-//   );
-// }
-//
-// function selectCursorParent() {
+// function moveCursorOneLineDown() {
 //   const selection = window.getSelection();
 //
 //   if (!selection || selection.rangeCount === 0) return;
 //
 //   const range = selection.getRangeAt(0);
-//   const startNode = range.startContainer;
-//   const endNode = range.endContainer;
+//   const currentNode = range.startContainer;
+//   const currentOffset = range.startOffset;
 //
-//   const lineRange = document.createRange();
+//   let nextNode: Node | null = currentNode;
+//   let nextOffset = currentOffset;
 //
-//   lineRange.setStartBefore(startNode);
-//   lineRange.setEndAfter(endNode);
+//   console.log("~ range", range);
+//   console.log("~ current node", currentNode);
+//   console.log("~ current offset", currentOffset);
+//   console.log("~ next node", currentNode.nextSibling);
 //
-//   let element: HTMLElement | null = startNode as HTMLElement;
-//
-//   if (element.nodeType === Node.TEXT_NODE) {
-//     element = startNode.parentElement;
-//   }
-//
-//   const isALineDiv = element?.tagName === "div";
-//
-//   if (isALineDiv) {
-//     element = (element?.children[0] as HTMLElement) ?? null;
-//   }
-//
-//   return element;
+//   // if (currentNode.nodeType === Node.TEXT_NODE) {
+//   //   // If we're at a text node, check for newlines and move to the next line if found
+//   //   const parentNode = currentNode.parentNode;
+//   //   let foundNewline = false;
+//   //
+//   //   while (nextNode && !foundNewline) {
+//   //     if (nextNode.nodeType === Node.TEXT_NODE) {
+//   //       nextOffset = nextNode.textContent?.length || 0;
+//   //       nextNode = nextNode.nextSibling;
+//   //     }
+//   //
+//   //     if (nextNode && nextNode.nodeType === Node.TEXT_NODE && nextNode.textContent?.includes("\n")) {
+//   //       foundNewline = true;
+//   //       nextOffset = (nextNode.textContent?.indexOf("\n") || 0) + 1;
+//   //       if (nextNode instanceof Text) {
+//   //         nextNode = nextNode.splitText(nextOffset);
+//   //       }
+//   //     }
+//   //   }
+//   //
+//   //   if (!foundNewline && parentNode) {
+//   //     nextNode = parentNode.nextSibling;
+//   //     nextOffset = 0;
+//   //   }
+//   // } else if (currentNode.nodeType === Node.ELEMENT_NODE) {
+//   //   // If current node is an element, check if it's a BR or empty element and handle accordingly
+//   //   if (currentNode.nodeName === "BR" || (currentNode as HTMLElement).innerText === "") {
+//   //     nextNode = currentNode.nextSibling;
+//   //   } else {
+//   //     nextNode = currentNode.childNodes[currentOffset] || currentNode.nextSibling;
+//   //   }
+//   // }
+//   //
+//   // // If nextNode is not a text node, find the next text node or a BR element
+//   // while (nextNode && nextNode.nodeType !== Node.TEXT_NODE && nextNode.nodeName !== "BR") {
+//   //   nextNode = nextNode.firstChild || nextNode.nextSibling;
+//   // }
+//   //
+//   // if (!nextNode) {
+//   //   return;
+//   // }
+//   //
+//   // // If the next node is a BR element, adjust the cursor position
+//   // if (nextNode.nodeName === "BR") {
+//   //   nextOffset = 0;
+//   // }
+//   //
+//   // // Set the cursor position to the next node and offset
+//   // const newRange = document.createRange();
+//   // newRange.setStart(nextNode, nextOffset);
+//   // newRange.collapse(true);
+//   //
+//   // selection.removeAllRanges();
+//   // selection.addRange(newRange);
 // }
-//
-// const saveCursorPosition = (el: HTMLElement): CursorPosition | null => {
+
+// function saveCursorPosition(el: HTMLElement): CursorPosition | null {
 //   const selection = window.getSelection();
 //
 //   if (!selection || selection.rangeCount === 0) return null;
 //
 //   const range = selection.getRangeAt(0);
 //   const preCaretRange = range.cloneRange();
-//
 //   preCaretRange.selectNodeContents(el);
 //   preCaretRange.setEnd(range.startContainer, range.startOffset);
 //
@@ -423,7 +278,7 @@ function restoreCursorPosition(el: HTMLElement, positions: CursorPosition | null
 //     start,
 //     end: start + range.toString().length,
 //   };
-// };
+// }
 //
 // function restoreCursorPosition(el: HTMLElement, positions: CursorPosition | null) {
 //   if (!positions) return;
@@ -451,6 +306,8 @@ function restoreCursorPosition(el: HTMLElement, positions: CursorPosition | null
 //     return null;
 //   }
 //
+//   setCursorPosition(el, positions);
+//
 //   function setCursorPosition(el: HTMLElement, cursorPosition: CursorPosition) {
 //     const selection = window.getSelection();
 //
@@ -461,7 +318,6 @@ function restoreCursorPosition(el: HTMLElement, positions: CursorPosition | null
 //
 //     if (start && end) {
 //       const range = document.createRange();
-//
 //       range.setStart(start.node, start.offset);
 //       range.setEnd(end.node, end.offset);
 //
@@ -469,6 +325,4 @@ function restoreCursorPosition(el: HTMLElement, positions: CursorPosition | null
 //       selection.addRange(range);
 //     }
 //   }
-//
-//   setCursorPosition(el, positions);
 // }
